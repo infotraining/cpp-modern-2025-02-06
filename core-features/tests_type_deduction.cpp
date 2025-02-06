69687e5f60470cfa95576bf2581b9deff3009843
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-02-06 13:33:10 +0100

    Enable auto declarations exercise and add related tests

--- core-features/tests_type_deduction.cpp
+++ core-features/tests_type_deduction.cpp
@@ -7,6 +7,7 @@
 #include <numeric>
 #include <string>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -14,13 +15,56 @@ using namespace std;
 #define __PRETTY_FUNCTION__ __FUNCSIG__
 #endif
 
-void foo(int)
-{ }
+/////////////////////////////////////////////
+// auto declarations
+
+uint64_t get_number()
+{
+    return 42;
+}
+
+TEST_CASE("auto declarations")
+{
+    auto x = 42;     // x : int
+    const auto y = 10.0;   // y : double
+    auto z = 10.0f;  // z : float
+    auto c_txt = "text"; // c_txt: const char* 
+    auto str = "text"s; // str: std::string
+    auto n = get_number(); // n : uint64_t
+
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    auto it = vec.begin();
+
+    auto& vec_item = vec[2];
+    vec_item = 42;
+    REQUIRE(vec[2] == 42);
+
+    auto* ptr = &x; // int*
+    const auto* ptr_to_const = &x; // const int*
+    const auto const_ptr = &x;     // int* const
+}
+
+void foo(int value);
+
+void foo(const int value)
+{ 
+    //++value;
+}
+
+
 
 template <typename T>
 void deduce1(T arg)
 {
     std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+
+namespace Cpp20
+{
+    void deduce1(auto arg)
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+    }
 }
 
 template <typename T>
@@ -45,36 +89,38 @@ TEST_CASE("type deduction rules")
 
     SECTION("Case 1")
     {
-        deduce1(x);
-        deduce1(cx);     
-        deduce1(ref_x);  
-        deduce1(cref_x); 
-        deduce1(tab);    
-        deduce1(foo);    
+        deduce1(x);   // deduce1<T = int>(int arg)
+        deduce1(cx);  // deduce1<T = int>(int arg) - const is removed
+        deduce1(ref_x);  // deduce1<T = int>(int arg) - ref is removed
+        deduce1(cref_x); // deduce1<T = int>(int arg) - const and ref is removed
+        deduce1(tab);    // deduce1<T = int*>(int* arg) - array decays to pointer
+        deduce1("text"); // deduce1<T = const char*>(const char*) - array decays to pointer
+        deduce1(foo);    // deduce1<T = void(*)(int)>(void(*arg)(int)) - function decays to function pointer 
 
-        auto a1 = x;
-        auto a2 = cx;
-        auto a3 = ref_x;
-        auto a4 = cref_x;
-        auto a5 = tab;
-        auto a6 = foo;
+        auto a1 = x;       // int
+        auto a2 = cx;      // int - const is removed
+        auto a3 = ref_x;   // int - ref is removed
+        auto a4 = cref_x;  // int - const and ref is removed
+        auto a5 = tab;     // int* - array decays to pointer
+        auto a6 = "text";  // const char*
+        auto a7 = foo;     // void(*)(int) - function decays to function pointer 
     }
 
     SECTION("Case 2")
     {
-        deduce2(x);      
-        deduce2(cx);     
-        deduce2(ref_x);  
-        deduce2(cref_x); 
-        deduce2(tab);    
-        deduce2(foo);    
+        deduce2(x);         // deduce2<T = int>(int& arg)
+        deduce2(cx);        // deduce2<T = const int>(const int& arg)
+        deduce2(ref_x);     // deduce2<T = int>(int& arg)
+        deduce2(cref_x);    // deduce2<T = const int>(const int& arg)
+        deduce2(tab);       // deduce2<T = int[10]>(int(&arg)[10])
+        deduce2(foo);       // deduce2<T = void(int)>(void (&arg)(int))
 
-        auto& a1 = x;
-        auto& a2 = cx;
-        auto& a3 = ref_x;
-        auto& a4 = cref_x;
-        auto& a5 = tab;
-        auto& a6 = foo;
+        auto& a1 = x;       // int&
+        auto& a2 = cx;      // const int&
+        auto& a3 = ref_x;   // int&
+        auto& a4 = cref_x;  // const int&
+        auto& a5 = tab;     // int(&)[10]
+        auto& a6 = foo;     // void(&)(int)
     }
 
     SECTION("Case 3")
