d308b5b4a5c6971e884134e86513f62524474682
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-02-12 11:15:34 +0000

    Implement move semantics for ContainerInt; add move constructor and move assignment functionality with tests

--- move-semantics/move_container_int.cpp
+++ move-semantics/move_container_int.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_section_info.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
+#include <utility>
 
 class ContainerInt
 {
@@ -26,6 +27,10 @@ public:
         , items_{new int[size_]}
     {
         std::copy(source.items_, source.items_ + size_, items_);
+
+        std::cout << "Container(cc: ";
+        print_items();
+        std::cout << ");\n";
     }
 
     // copy assignment
@@ -39,6 +44,10 @@ public:
             items_ = new int[size_];
 
             std::copy(source.items_, source.items_ + size_, items_);
+
+            std::cout << "Container(ca: ";
+            print_items();
+            std::cout << ");\n";
         }
 
         return *this;
@@ -46,11 +55,12 @@ public:
 
     // TODO: move constr
     ContainerInt(ContainerInt&& source)
-        : size_{source.size_}
-        , items_{source.items_}
+        : size_{std::exchange(source.size_, 0)}
+        , items_{std::exchange(source.items_, nullptr)}
     {
-        source.size_ = 0;
-        source.items_ = nullptr;
+        std::cout << "Container(mv: ";
+        print_items();
+        std::cout << ");\n";
     }
 
     // TODO: move assignment
@@ -60,11 +70,12 @@ public:
         {
             delete[] items_;
 
-            size_ = source.size_;
-            items_ = source.items_;
+            size_ = std::exchange(source.size_, 0);
+            items_ = std::exchange(source.items_, nullptr);
 
-            source.size_ = 0;
-            source.items_ = nullptr;
+            std::cout << "Container(ma: ";
+            print_items();
+            std::cout << ");\n";
         }
 
         return *this;
@@ -126,7 +137,91 @@ public:
 private:
     size_t size_;
     int* items_;
+
+    void print_items() const
+    {
+        for (const auto& item : *this)
+        {
+            std::cout << item << " ";
+        }
+    }
 };
+
+namespace ModernCpp
+{
+    class ContainerInt
+    {
+    public:
+        ContainerInt(size_t size, int value)
+            : items_(size, value)
+        {
+        }
+
+        ContainerInt(std::initializer_list<int> values)
+            : items_(values)
+        {
+        }
+
+        int& operator[](size_t index)
+        {
+            return items_[index];
+        }
+
+        const int& operator[](size_t index) const
+        {
+            return items_[index];
+        }
+
+        size_t size() const
+        {
+            return items_.size();
+        }
+
+        using iterarator = std::vector<int>::iterator;
+        using const_iterator = std::vector<int>::const_iterator;
+
+        iterarator begin()
+        {
+            return items_.begin();
+        }
+
+        iterarator end()
+        {
+            return items_.end();
+        }
+
+        const_iterator begin() const
+        {
+            return items_.begin();
+        }
+
+        const_iterator end() const
+        {
+            return items_.end();
+        }
+
+        const_iterator cbegin() const
+        {
+            return items_.cbegin();
+        }
+
+        const_iterator cend() const
+        {
+            return items_.cend();
+        }
+
+    private:
+        std::vector<int> items_;
+
+        void print_items() const
+        {
+            for (const auto& item : *this)
+            {
+                std::cout << item << " ";
+            }
+        }
+    };
+}
 
 template <typename T>
 void print_all(const T& container, std::string_view prefix)
@@ -149,7 +244,7 @@ TEST_CASE("implement move semantics")
         ContainerInt c1 = {10, 42};
         print_all(c1, "c1");
 
-        ContainerInt c2 = std::move(c1); // move constructor
+        ContainerInt c2{std::move(c1)}; // move constructor // ContainerInt{ContainerInt&&}
 
         std::cout << "After move construction" << std::endl;
         print_all(c1, "c1");
@@ -172,7 +267,7 @@ TEST_CASE("implement move semantics")
         print_all(c2, "c2");
 
         c2 = std::move(c1); // move assignment
-
+        // c2 = static_cast<ContainerInt&&>(c1); // std::move is static_cast to rvalue-ref
         std::cout << "After move assignment" << std::endl;
         print_all(c1, "c1");
         print_all(c2, "c2");
@@ -181,5 +276,63 @@ TEST_CASE("implement move semantics")
         REQUIRE(c2.size() == 2);
         REQUIRE(c2[0] == 10);
         REQUIRE(c2[1] == 42);
+
+        ContainerInt c3 = std::exchange(c2, ContainerInt{665, 667});
     }
+}
+
+TEST_CASE("std::move")
+{
+    SECTION("for primitive types - makes copy")
+    {
+        int x = 42;
+        int y = std::move(x); // copy of x to y
+        REQUIRE(x == y);
+
+        int* ptr_x = &x;
+        int* ptr_y = std::move(ptr_x); // copy of ptr_x to ptr_y
+        REQUIRE(ptr_x == ptr_y);
+    }
+
+    SECTION("for custom/library")
+    {
+        std::vector<int> vec = {1, 2, 3, 4};
+        std::vector<int> target = std::move(vec);
+        REQUIRE(vec.size() == 0);
+        REQUIRE(target == std::vector{1, 2, 3, 4});
+
+        SECTION("const object is copied")
+        {
+            const std::vector<int> vec = {1, 2, 3, 4};
+            std::vector<int> target = std::move(vec);
+            REQUIRE(target == vec);
+        }
+    }
+}
+
+struct MyData
+{
+    int id{};
+    std::string name;
+    ContainerInt data = {1, 2, 3};
+
+    inline static std::string class_id = "MyData";
+
+    MyData() = default;
+
+    MyData(int id, std::string name, ContainerInt data)
+        : id{id}
+        , name{std::move(name)}
+        , data{std::move(data)}
+    {
+    }
+};
+
+TEST_CASE("MyData - default special ops")
+{
+    MyData md1{1, "one", {1, 2, 3}};
+
+    MyData md2 = md1; // cc
+
+    MyData md3 = std::move(md1); // mv
 }
