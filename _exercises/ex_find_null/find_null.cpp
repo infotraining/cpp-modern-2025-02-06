b1afde8e914c301bfd6099e523d7dbca56d69d2f
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-02-12 09:07:37 +0000

    Refactor find_null implementation and enhance test cases; introduce custom Container and alternative find_null function

--- _exercises/ex_find_null/find_null.cpp
+++ _exercises/ex_find_null/find_null.cpp
@@ -1,23 +1,75 @@
-#include <catch2/catch_test_macros.hpp>
-
 #include <cassert>
+#include <catch2/catch_test_macros.hpp>
 #include <iostream>
 #include <iterator>
 #include <memory>
 #include <vector>
 
-using namespace std;
+namespace Custom
+{
+    struct Container
+    {
+        std::vector<int> items;
 
-// TODO
+    };
+    
+    auto begin(Container& c) { return c.items.begin(); }
+    
+    auto end(Container& c) { return c.items.end(); }
 
+    void use(Container& c)
+    {
+        std::cout << "Container::use()" << "\n";
+    }
+}
+
+template <typename T>
+auto find_null(T& cont)
+{
+    using std::begin;
+    using std::end;
+    auto it = begin(cont);
+    for (; it != end(cont); it++)
+    {
+        if (*it == nullptr)
+        {
+            return it;
+        }
+    }
+
+    return it;
+}
+
+// ADL - Argument Dependent Lookup
+TEST_CASE("custom")
+{
+    Custom::Container c{};
+    Custom::use(c);
+
+    use(c); // ADL
+}
+
+namespace Alternative
+{
+    template <typename T>
+    auto find_null(T& container)
+    {
+        using std::begin;
+        using std::end;
+        //return std::find_if(begin(container), end(container), [](const auto& ptr) { return ptr == nullptr; });
+        return std::find(begin(container), end(container), nullptr);
+    }
+}
 
 TEST_CASE("find_null description")
 {
+    using namespace std;
+
     SECTION("finds first null pointer in a std container of raw pointers")
     {
         vector<int*> ptrs = {new int{9}, new int{10}, nullptr, new int{20}, nullptr, new int{23}};
 
-        vector<int*>::iterator first_null_appearence = find_null(ptrs);
+        vector<int*>::iterator first_null_appearence = Alternative::find_null(ptrs);
 
         REQUIRE(distance(ptrs.begin(), first_null_appearence) == 2);
 
@@ -30,7 +82,7 @@ TEST_CASE("find_null description")
     {
         int* ptrs[] = {new int{9}, new int{10}, NULL, new int{20}, nullptr, new int{23}};
 
-        auto first_null_appearence = find_null(ptrs);
+        auto first_null_appearence = Alternative::find_null(ptrs);
 
         REQUIRE(distance(begin(ptrs), first_null_appearence) == 2);
 
