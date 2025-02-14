35ef5c1e140f83780122354265b5e85a9a5c2ba2
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-02-14 09:14:49 +0000

    Implement FwdList using std::unique_ptr with push_front, pop_front, and size methods

--- _exercises/ex-unique-ptr/fwd_list.hpp
+++ _exercises/ex-unique-ptr/fwd_list.hpp
@@ -97,7 +97,81 @@ namespace ModernCpp
     template <typename T>
     class FwdList
     {
-        // TODO: use std::unique_ptr & implement FwdList
+    private:
+        struct Node
+        {
+            T value;
+            std::unique_ptr<Node> next;
+        };
+    public:
+        FwdList()  = default;  
+
+        FwdList(const FwdList&) = delete;
+        FwdList& operator=(const FwdList&) = delete;
+        FwdList(FwdList&&) = default;
+        FwdList& operator=(FwdList&&) = default;
+        
+        ~FwdList() noexcept
+        {
+            while (head_)
+            {
+                head_ = std::move(head_->next);
+            }
+        }
+
+        bool empty() const
+        {
+            return size_ == 0;
+        }
+
+        size_t size() const
+        {
+            return size_;
+        }
+
+        void push_front(const T& item)
+        {
+            auto new_node = std::make_unique<Node>(item, std::move(head_));
+            head_ = std::move(new_node);
+            ++size_;
+        }
+
+        T& front() const
+        {
+            assert(head_ != nullptr);
+            return head_->value;
+        }
+
+        void pop_front()
+        {
+            assert(size_ != 0);
+
+            head_ = std::move(head_->next);
+
+            --size_;
+        }
+
+        friend std::ostream& operator<<(std::ostream& out, const FwdList<T>& lst)
+        {
+            out << "[";
+            const auto* node = lst.head_.get();
+
+            while(node)
+            {
+                out << node->value;
+                node = node->next.get();
+
+                if (node)
+                    out << ", ";
+            }
+
+            out << "]";
+
+            return out;
+        }
+    private:
+        size_t size_{0};
+        std::unique_ptr<Node> head_{nullptr};
     };
 }
 
